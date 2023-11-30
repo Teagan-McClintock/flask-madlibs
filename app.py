4276683b77caf15774778bd7944bcec2c7c284b0
Brandie Lucano <blucano.dev@gmail.com>
2023-11-29 16:28:06 -0800

    madlibs working before further study

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from stories import silly_story
 
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
@@ -10,5 +11,15 @@ debug = DebugToolbarExtension(app)
 
 @app.get("/")
 def show_question_form():
+    """Generate and show form to ask for all prompts"""
+
     prompts = silly_story.prompts
-    return render_template("questions.html", prompts = prompts)
+    return render_template("questions.html", prompts=prompts)
+
+@app.get("/results")
+def show_story():
+    """Show story result."""
+
+    story_text = silly_story.get_result_text(request.args)
+    return render_template("results.html", story_text=story_text)
+
